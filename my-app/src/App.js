5709af02002dbc4342a219ff588658e2eb13b1c7
JGeho <jgeho@hotmail.com>
2021-11-09 20:23:23 -0500

    working site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,19 @@
 import React from 'react';
-import Search from './components/Search';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
-import Results from './components/Results';
-import Saved from './components/Saved';
 
 function App(props) {
   return (
     <div className="App">
       <Router>
         <Navbar/>
-        <Search/>
         <Switch>
           <Route exact path="/">
             <Home/>
           </Route>
         </Switch>
-        <Route path="/results">
-            <Results/>
-        </Route>
-        <Route path="/saved">
-            <Saved/>
-        </Route>        
       </Router>
     </div>
   );
--- my-app/src/components/Results.js
+++ my-app/src/components/Results.js
@@ -1,63 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
-import {useDispatch} from 'react-redux';
 
 function Results(props) {
-    const [bookData,setBookData] = useState([]);
-    const dispatch = useDispatch();
-
-    const fetchBooks = () =>{
-        return axios.get("https://mocki.io/v1/7be8eb26-eb72-4ddb-9925-80d157ed2252")
-        .then(books=>books.data);
-    }
-
-    useEffect(()=>{
-        fetchBooks().then(bookResponse=>{
-            setBookData(bookResponse['books']);
-        })
-    },[])
-
-    const handleCheckout = () =>{
-        const checkBoxes = document.querySelectorAll("input[type=checkbox]");
-        for(let index=0; index<checkBoxes.length; index++){
-            if(checkBoxes[index].checked){
-                let selectedBooks = bookData.filter(function(book) {
-                    return book.isbn === checkBoxes[index].value})
-                    selectedBooks.map((selectedBook)=>{
-                        return dispatch({type:'CHECK_OUT',isbns:selectedBook})
-                    })
-
-            }
-        }
-    }
     return (
         <div className="container">
             <table className="table">
                 <thead>
                     <tr>
-                        <th>Select</th>
-                        <th>ISBN</th>
-                        <th>Book Name</th>
-                        <th>Author</th>
-                        <th>Description</th>
+                        <th>Name</th>
+                        <th>Street</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>Zip Code</th>
+                        <th>Phone</th>
+                        <th>Website</th>
                     </tr>
                 </thead>
                 <tbody>
-                {
-                    bookData.map((book,index)=>(
-                        <tr key={index}>
-                            <td><input type="checkbox" value={book.isbn}/></td>
-                            <td>{book.isbn}</td>
-                            <td>{book.book_name}</td>
-                            <td>{book.author_name}</td>
-                            <td>{book.description}</td>
-                        </tr>
-                    ))
-                }
+                    {
+                        props.beerData.map((data, index)=>(
+                            <tr key={index}>
+                                <td>{data?.name}</td>
+                                <td>{data?.street}</td>
+                                <td>{data?.city}</td>
+                                <td>{data?.state}</td>
+                                <td>{data?.postal_code}</td>
+                                <td>{data?.phone}</td>
+                                <td>{data?.website_url}</td>
+                            </tr>
+                        ))                     
+                    }
                 </tbody>
             </table>
-            <button onClick={handleCheckout}>Add to Saved</button>
         </div>
     );
 }
